f740a0919cc7ca8f122575339d947c048e92054c
abhishekbaraik333 <abhitamangme@gmail.com>
2025-01-31 12:55:10 +0530

    I hv updated

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ document.addEventListener("DOMContentLoaded", function () {
     hamburger.classList.toggle("active");
   });
 });
+
+
+const section = document.querySelector('.guestbook-section');
+window.addEventListener('scroll', () => {
+  const sectionTop = section.getBoundingClientRect().top;
+  const triggerPoint = window.innerHeight / 1.2;
+  if (sectionTop < triggerPoint) {
+    section.classList.add('visible');
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const revealElements = document.querySelectorAll(".reveal");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  revealElements.forEach((element) => observer.observe(element));
+});
